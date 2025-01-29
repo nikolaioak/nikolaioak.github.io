337d9f9709e86c3f1fad12b4d539ce32b547503c
Nikolai Oakfield <foyer_solaria_0q@icloud.com>
2025-01-28 23:08:01 -0500

    added grid formatting for beer gallery

---
+++ js/beer.js
@@ -0,0 +1,19 @@
+let  beerContainer = document.getElementById('beerContainer');
+async function getBeers() {
+    const response = await fetch('https://nikolaimakes.beer/beer-data.json');
+    const json = await response.json();
+    return json.beerData;
+};
+
+getBeers().then(beers => {
+    let beerHTML = '';
+    let beerUrl = new URL('https://nikolaimakes.beer/beer-details.html');
+    beers.forEach(beer => {
+        let beerDt = new Date(Date.parse(beer.startDate));
+        let beerMon = beerDt.toLocaleString('default', { month: 'short' });
+        let beerYr = beerDt.getFullYear();
+        beerUrl.searchParams.append('beer', beer.id);
+        beerHTML += `<div class="beer-item"><a href="${beerUrl}"><img src="./img/${beer.beerType}.ico" alt="${beer.name}"/><h4>${beer.name}</h4></a><p>${beer.beerType} &bull; ${beerMon} ${beerYr}</p></div>`;
+    });
+    beerContainer.innerHTML += beerHTML;
+});
