dcd5e2464c0f465974c26f897133082d66c438c9
Nikolai Oakfield <foyer_solaria_0q@icloud.com>
2025-01-29 21:28:28 -0500

    add functionality to parse beer data for this beer's info and display it on the beer details page

---
+++ js/beer-details.js
@@ -0,0 +1,65 @@
+let beerTitle = document.getElementById('beer-name'),
+    statsContainer = document.getElementById('stats-container'),
+    ingredContainer = document.getElementById('ingredients-container'),
+    blurbContainer = document.getElementById('blurb-container');
+
+async function getBeers() {
+    const response = await fetch('https://nikolaimakes.beer/beer-data.json');
+    const json = await response.json();
+    return json.beerData;
+};
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+let beerId = params.beer;
+
+getBeers().then(beers => {
+    let statsHTML = '';
+    let ingredHTML = '';
+    let blurbHTML =  '';
+    beers.forEach(beer => {
+        if (beer.id === beerId) {
+            statsHTML += `<table>
+                            <tr>
+                                <th>I made this...</th>
+                                <th>Type</th>
+                                <th>IBU</th>
+                                <th>OG</th>
+                                <th>SG</th>
+                                <th>ABV</th>
+                            </tr>
+                            <tr>
+                                <td>${beer.startDate} - ${beer.endDate}</td>
+                                <td>${beer.beerType}</td>
+                                <td>${beer.ibu}</td>
+                                <td>${beer.originalGravity}</td>
+                                <td>${beer.specificGravity}</td>
+                                <td>${(beer.originalGravity - beer.specificGravity)*131.25} %</td>
+                            </tr>
+                        </table>`;
+            beer.ingredients.forEach((ing, idx) => {
+                if (idx == 0) {
+                    ingredHTML  += `<table>`;
+                }
+                ingredHTML += `<tr>
+                                <td>${ing.quantity}</td>
+                                <td>${ing.uom}</td>
+                                <td>${ing.name}</td>
+                            </tr>`;
+                if (idx == beer.ingredients.length) {
+                    ingredHTML  += `</table>`;
+                }
+            });
+            beer.beerBlurb.forEach(blurb => {
+                blurbHTML += `<p>${blurb}</p><br>`;
+            });
+        }
+    });
+    beerTitle.innerText =  beerTitle;
+    statsContainer.innerHTML += statsHTML;
+    ingredContainer.innerHTML += ingredHTML;
+    blurbContainer.innerHTML += blurbHTML;
+});
+
