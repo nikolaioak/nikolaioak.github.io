857713f6cd6646d2086888bcb7727a40d8d95abd
Nikolai Oakfield <foyer_solaria_0q@icloud.com>
2025-01-30 20:07:35 -0500

    add date back to beer page and fix styling

--- js/beer.js
+++ js/beer.js
@@ -9,8 +9,11 @@ getBeers().then(beers => {
     let beerHTML = '';
     let beerUrl = new URL('https://nikolaimakes.beer/beer-details.html');
     beers.forEach(beer => {
+        let beerDt = new Date(Date.parse(beer.startDate));
+        let beerMon = beerDt.toLocaleString('default', { month: 'short' });
+        let beerYr = beerDt.getFullYear();
         beerUrl.searchParams.set('beer', beer.id);
-        beerHTML += `<div class="beer-item"><a href="${beerUrl}"><img src="./img/nikolaimakesbeer-favicon.ico" alt="${beer.name}"/><h4>${beer.name}</h4></a></div>`;
+        beerHTML += `<div class="beer-item"><a href="${beerUrl}"><img src="./img/nikolaimakesbeer-favicon.ico" alt="${beer.name}"/><p>${beer.name}</p></a><p class="beer-date">${beerMon} ${beerYr}</p></div>`;
     });
     beerContainer.innerHTML += beerHTML;
 });
