1756305720c7dc5095c7c301dbb116861738b1d3
Nikolai Oakfield <foyer_solaria_0q@icloud.com>
2025-02-08 11:43:09 -0500

    new tinkering details page gathering info from tinkering data json

---
+++ js/tinkering-details.js
@@ -0,0 +1,44 @@
+let tinkeringTitle = document.getElementById('tinkering-title'),
+    dateContainer = document.getElementById('date-container'),
+    blurbContainer = document.getElementById('blurb-container'),
+    picsContainer = document.getElementById('pics-container');
+
+async function getTinkering() {
+    const response = await fetch('https://nikolaimakes.beer/tinkering-data.json');
+    const json = await response.json();
+    return json.tinkeringData;
+};
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+let tinkeringId = params.tinkering;
+
+getTinkering().then(tinkerings => {
+    let blurbHTML =  '';
+    let picsHTML = '';
+    tinkerings.forEach(tinkering => {
+        if (tinkering.id === tinkeringId) {
+            tinkeringTitle.innerHTML =  tinkering.title;
+            let tinkeringDt = new Date(Date.parse(tinkering.entryDate));
+            let tinkeringDay = tinkeringDt.getDay();
+            let tinkeringMon = beerDt.toLocaleString('default', { month: 'short' });
+            let tinkeringYr = beerDt.getFullYear();
+            dateContainer.innerHTML += `<h4>${tinkeringDay} ${tinkeringMon} ${tinkeringYr}</h4>`;
+            tinkering.tinkeringBlurb.forEach(blurb => {
+                blurbHTML += `<p>${blurb}</p>`;
+            });
+
+            tinkering.tinkeringPics.forEach(pic => {
+                picsHTML += `<div class="tinkering-pic-item">
+                                    <img src="${pic.imgPath}" alt="${pic.alt}"/>
+                                    <p>${pic.subtitle}</p>
+                            </div>`;
+            });
+        }
+    });
+    blurbContainer.innerHTML += blurbHTML;
+    picsContainer.innerHTML += picsHTML;
+});
+
